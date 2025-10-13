c6892310079690893c65ee7afec431f41d80fec5
pr1zmo <zaidbassari@gmail.com>
2025-10-13 14:30:30 +0100

    M

--- starting/ex02/find_ft_type.py
+++ starting/ex02/find_ft_type.py
@@ -1,2 +1,9 @@
 def all_thing_is_obj(object : any) -> int:
+	if (type(object) == str):
+		print(object + " is in the kitchen", end=" ")
+	else:
+		res = str(type(object))
+		res = res.replace("<class '", "")
+		res = res.replace("'>", "")
+		print(res + ": ", end="")
 	print(type(object))
