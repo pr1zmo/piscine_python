ec37264a1319e81f8d256314ec4b2971cb68c78f
pr1zmo <zaidbassari@gmail.com>
2025-10-13 15:28:31 +0100

    M

---
+++ starting/ex05/building.py
@@ -0,0 +1,43 @@
+import sys
+
+def handle_input(inp : any) -> any:
+	word_count = 0
+	upper = 0
+	lower = 0
+	punc = 0
+	space = 0
+	digits = 0
+	for i in inp:
+		if (i.isdigit()):
+			digits += 1
+		elif (i.islower()):
+			lower += 1
+		elif (i.isupper()):
+			upper += 1
+		elif (i == ' '):
+			space += 1
+		else:
+			punc += 1
+		word_count+=1
+	print(f"The text contains {word_count} characters: ")
+	print(f"{upper} upper letters")
+	print(f"{lower} lower letters")
+	print(f"{punc} punctuation marks")
+	print(f"{space} spaces")
+	print(f"{digits} digits")
+
+def building() -> any:
+	if (len(sys.argv) > 1):
+		inp = sys.argv[1]
+	else:
+		inp = ""
+	print("Enter text (press Ctrl+D when done):")
+	try:
+		inp = sys.stdin.read()  # Reads until EOF
+	except KeyboardInterrupt:
+		print("\nInput interrupted.")
+		return
+	if (len(sys.argv) > 2):
+		raise AssertionError("Only takes one argument")
+	return handle_input(inp)
+	
---
+++ starting/ex05/main.py
@@ -0,0 +1,4 @@
+from building import building
+
+if __name__ == "__main__":
+	building()
